bc563f1e5d8282bc5692c274cb9bdfff342500a9
vikram mahara <vmahara1999@gmail.com>
2022-03-08 00:02:10 +0530

    first commit

---
+++ js/slider.js
@@ -0,0 +1,46 @@
+var windowWidth = 0;
+$(window).on('load', initSlider);
+function initSlider() {    
+    if ($(window).width() === windowWidth) return;
+	$(window).on('resize', initSlider);
+    windowWidth = $(window).width();
+    $(".Slider").each(function () {
+        var s = $(this);
+        
+        s.find(".cycle-next,.cycle-prev").addClass("d-none");
+        s.find(".slideshow").each(function () {
+            var ss = $(this);
+            ss.css("maxHeight", "none");
+            if (ss.find(".cycle-sentinel").length > 0)
+                ss.cycle('destroy').find(".cycle-sentinel").remove();
+
+            var ssw = ss.outerWidth();
+            var is = ss.find(">.isItem");
+            if (is.length > 0) {
+                var iw = is.first().outerWidth();
+                var ih = is.first().outerHeight();
+                var isw = iw * is.length;
+                var cv = Math.round(ssw / iw);
+
+                ss.css("maxHeight", ih + "px");
+                if (isw > ssw) {
+                    iw = parseInt(ssw / cv);
+
+                    is.css("width", iw).css("float", "none");
+                    s.find(".cycle-next,.cycle-prev").removeClass("d-none");
+
+                    ss.cycle({
+                        fx: "carousel",
+                        log: false,
+                        slides: ">.isItem",
+                        timeout:0,
+                        next: s.find(".cycle-next"),
+                        prev: s.find(".cycle-prev"),
+                        carouselVisible: cv
+                    });
+
+                }
+            }
+        });
+    });
+}
